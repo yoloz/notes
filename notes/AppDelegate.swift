0d1ee74c4fa73bb7561b2b968b3c233f6243c969
yoloz <yoloz@yolozs-mac.local>
2020-08-29 14:07:34 +0800

    Initial Commit

---
+++ notes/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  notes
+//
+//  Created by yoloz on 2020/8/29.
+//  Copyright © 2020 yoloz. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
